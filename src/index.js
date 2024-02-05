0b5de4b64ea8066c55bdc4f1865c17a394320689
Chandu-Develop <bhuvanchandumidde3@gmail.com>
2024-02-05 19:01:34 +0530

    deploying

--- src/index.js
+++ src/index.js
@@ -1,36 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const Box = (props) => (
+const Box = () => {
   <>
     <div className='box'>
-      <h1>{props.name}</h1>
+      <h1>Hello</h1>
     </div>
   </>
-)
+}
 
 const App = () => {
-  const name = "chandu"
-  const age = 20
-  const height = 5.9
-  const degree = "B.Tech"
   return (
     <>
-      <div
-        name={name}
-        age={age}
-        height={height}
-        degree={degree}
-      >
-      </div>
-      <Box name={name} age={age} height={height} degree={degree} />
+      <Box />
     </>
   )
 }
 
 root.render(
-  <App />
+  <App name="Chandu" age="20" degree="B.Tech" />
 );
 
